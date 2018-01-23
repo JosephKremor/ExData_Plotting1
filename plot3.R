8e7548bf03b87a597a36691ec12cd72d5bc18dd4
JosephKremor <33541306+JosephKremor@users.noreply.github.com>
2018-01-23 16:14:02 +0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -24,7 +24,7 @@ Data = Data[,Sub_metering_3:=as.numeric(Sub_metering_3)]
 
 # plotting# 
 
-png('plot3.png',height = 480,width = 480,units = px)
+png('plot3.png',height = 480,width = 480,units = 'px')
 plot(Data$datetime,Data$Sub_metering_1,ylab = 'Energy sub metering',type='l',xlab='')
 lines(Data$datetime,Data$Sub_metering_2,type='l',col = 'red')
 lines(Data$datetime,Data$Sub_metering_3,type='l',col = 'blue')
